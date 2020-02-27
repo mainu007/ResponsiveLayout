1680ab9f379cbc7cce8ec5807c0c229fe83ce3ee
mainu <mainudev.co@gmail.com>
2020-02-27 21:48:36 +0600

    Datepicker add on Home page and some style change

--- static/js/main.js
+++ static/js/main.js
@@ -24,6 +24,48 @@ function navbarChange() {
 window.addEventListener("scroll", navbarChange);
 //topnav code end
 
+//datepicker start
+//datepicker start
+$(document).ready(function() {
+  var nowTemp = new Date();
+  var now = new Date(
+    nowTemp.getFullYear(),
+    nowTemp.getMonth(),
+    nowTemp.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+  var checkin = $("#startDate")
+    .datepicker({
+      onRender: function(date) {
+        return date.valueOf() < now.valueOf() ? "disabled" : "";
+      }
+    })
+    .on("changeDate", function(ev) {
+      if (ev.date.valueOf() > checkout.date.valueOf()) {
+        var newDate = new Date(ev.date);
+        newDate.setDate(newDate.getDate() + 1);
+        checkout.setValue(newDate);
+      }
+      checkin.hide();
+      $("#endDate")[0].focus();
+    })
+    .data("datepicker");
+  var checkout = $("#endDate")
+    .datepicker({
+      onRender: function(date) {
+        return date.valueOf() <= checkin.date.valueOf() ? "disabled" : "";
+      }
+    })
+    .on("changeDate", function(ev) {
+      checkout.hide();
+    })
+    .data("datepicker");
+});
+//datepicker end
+
 //guest and room count code start
 //guest and room container code
 document.getElementById("roomGuestBarBtn").addEventListener("click", () => {
