09bef50b1819936b1ebd5167eacfb38a3eba58ca
Mainu <mainudev.co@gmail.com>
2020-03-11 11:14:08 +0600

    Added sign up section

--- static/js/sign_in.js
+++ static/js/sign_in.js
@@ -1,8 +1,25 @@
 function rememberActive() {
-  let btn = document.getElementById("rememberbtn");
-  function active() {
-    btn.classList.toggle("active");
+  let button = document.querySelectorAll(".rememberbtn");
+  function active(index) {
+    button[index].classList.toggle("active");
   }
-  btn.addEventListener("click", active);
+  button.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      active(index);
+    });
+  });
 }
 rememberActive();
+
+function buttonAnimationLoading() {
+  let button = document.querySelectorAll(".social_btn");
+  button.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      button.forEach(elementClass => {
+        elementClass.classList.remove("active");
+      });
+      element.className += " active";
+    });
+  });
+}
+buttonAnimationLoading();
